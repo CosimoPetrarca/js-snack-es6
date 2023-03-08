c3a07f1fefa1611448654378ae9e03941b81e114
P4anix <pancok_1@yahoo.it>
2023-03-08 16:04:47 +0100

    array di oggetti studenti

--- Snack 2/js/main.js
+++ Snack 2/js/main.js
@@ -1,1 +1,40 @@
 'Use strict'
+
+//creo un array di oggetti che rappresentano gli studenti (id,nome e voti)
+const studenti = [
+    {
+        id: 213,
+        nome: "Marco della Rovere",
+        voti: 78
+    },
+    {
+        id : 110,
+        nome: "Paola Cortellessa",
+        voti: 96
+    },
+    {
+        id : 250,
+        nome: "Andrea Mantegna",
+        voti: 48
+    },
+    {
+        id : 145,
+        nome: "Gaia Borromini",
+        voti: 74
+    },
+    {
+        id : 196,
+        nome: "Luigi Grimaldello",
+        voti: 68
+    },
+    {
+        id : 102,
+        nome: "Piero della Francesca",
+        voti: 50
+    },
+    {
+        id : 120,
+        nome: "Francesca da Polenta",
+        voti: 84
+    }
+];

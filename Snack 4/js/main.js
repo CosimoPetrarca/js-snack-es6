0f2d6b0692ca11a4edc5524d47a1f8b20dad72e6
P4anix <pancok_1@yahoo.it>
2023-03-09 17:18:57 +0100

    numeri randomici con il ciclo for

--- Snack 4/js/main.js
+++ Snack 4/js/main.js
@@ -1,1 +1,47 @@
 'Use strict'
+
+//creo un array di oggetti di squadre di calcio
+const squadre = [
+    {
+        nome: "Milan",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Inter",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Juventus",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Roma",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Napoli",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Lazio",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Atalanta",
+        punti: 0,
+        falli: 0
+    }
+];
+
+//genero numeri randomici per le proprietà punti e falli
+for (let i = 0; i < squadre.length; i++) {
+    squadre[i].punti = Math.floor(Math.random() * 90);
+    squadre[i].falli = Math.floor(Math.random() * 200);
+}
+console.log(squadre);

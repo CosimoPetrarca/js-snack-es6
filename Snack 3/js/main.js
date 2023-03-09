f90992a70d81796df5b47d8ff36640aeb96625f8
P4anix <pancok_1@yahoo.it>
2023-03-09 16:30:52 +0100

    bici leggera

--- Snack 3/js/main.js
+++ Snack 3/js/main.js
@@ -1,1 +1,35 @@
 'Use strict'
+
+//creo un array di oggetti bici da corsa con le proprietà nome e peso
+const biciclette = [
+    {
+        nome: "Bianchi",
+        peso: 7
+    },
+    {
+        nome: "Bmx",
+        peso: 6
+    },
+    {
+        nome: "Atala",
+        peso: 8
+    },
+    {
+        nome: "Cinzia",
+        peso: 12
+    },
+    {
+        nome: "Nevi",
+        peso: 10
+    },
+];
+
+//ciclo e trovare la bici con il peso minore
+let biciLeggera = biciclette[0];
+
+for (let i = 1; i < biciclette.length; i++) {
+  if (biciclette[i].peso < biciLeggera.peso) {
+    biciLeggera = biciclette[i];
+  }
+}
+console.log(biciLeggera);
